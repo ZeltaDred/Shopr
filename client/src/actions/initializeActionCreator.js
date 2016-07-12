e9fb1bb18589bcf6daaa1f1af5605349f45b3a90
ZeltaDred <douglas.osborn@dmail.dixie.edu>
2016-07-12 15:19:29 -0600

    Create initializeActionCreator.js
    intial commit   cloned   todo version  and  modified   variable names

---
+++ client/src/actions/initializeActionCreator.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var Dispatcher = require('../dispatcher/Dispatcher');
+var API = require('../helpers/api');
+var ActionTypes = require('../constants/actionTypes');
+
+var InitializeActionCreator = {
+
+	initializeApp: function () {
+		var storesPromise = API.getAllStores();
+
+		storesPromise
+			.then(function (stores) {
+				Dispatcher.dispatch({
+					actionType: ActionTypes.INITIALIZE,
+					initialData: {
+						stores: stores
+					}
+				});
+			});
+	}
+};
+
+module.exports = InitializeActionCreator;
