087c213fa476ebf8e38a461a2e337cb2692aadfd
Jake Duckett <Legac3e@gmail.com>
2016-07-13 14:35:33 -0600

    commented weird code

--- client/src/actions/shoppingActionCreator.js
+++ client/src/actions/shoppingActionCreator.js
@@ -8,18 +8,18 @@ var ShoppingActionCreator = {
 	createStore: function (store) {
 		var newStorePromise = API.createStore(store);
 
-<<<<<<< HEAD
-		newStorePromise.
-			then( function(newStore) {
-=======
-		newStorePromise
-		.then(function(newStore) {
->>>>>>> origin/matt-branch
-				Dispatcher.dispatch({
-				actionType: ActionTypes.CREATE_STORE,
-				store: newStore
-			});
-		})
+    // <<<<<<< HEAD
+    // newStorePromise.
+    //		then( function(newStore) {
+    // =======
+    //newStorePromise
+    // .then(function(newStore) {
+    // >>>>>>> origin/matt-branch
+    // 			Dispatcher.dispatch({
+    // 			actionType: ActionTypes.CREATE_STORE,
+    // 		store: newStore
+    //		});
+    //})
 		.fail(function(xhr, status, err) {
 			console.log("failed to create a store");
 		})
