3b0a924a3b915777ce9240206b784e92a0eb9e37
Jake Duckett <Legac3e@gmail.com>
2016-07-13 14:20:18 -0600

    commit

--- client/src/components/shopping/StoreForm.js
+++ client/src/components/shopping/StoreForm.js
@@ -2,7 +2,6 @@
 
 var React = require('react');
 var TextInput = require('../common/TextInput');
-var Link = require('react-router').Link;
 
 var StoreForm = React.createClass({
 
@@ -20,23 +19,13 @@ var StoreForm = React.createClass({
 					saveStoreState={this.props.saveStoreState}
 					error={this.props.errors.storeName}
 				/>
-<<<<<<< HEAD
-        <Link className="btn btn-primary btn-sm" to="/">
-=======
-				<Link className="btn btn-primary btn-sm"
-					to="/"
->>>>>>> origin/matt-branch
+        <Link className="btn btn-primary btn-sm" to="/"
 					onClick={this.props.saveStore} value="save">
 					Save Store &nbsp;
 					<span className="glyphicon glyphicon-save" aria-hidden="true"></span>
 				</Link>
 
-<<<<<<< HEAD
         <Link className="btn btn-primary btn-sm" to={"/shopping-list" + store._id}
-=======
-				<Link className="btn btn-primary btn-sm"
-					to={"/shopping-list" + store._id}
->>>>>>> origin/matt-branch
 					onClick={this.props.saveStore}>
 					Create List &nbsp;
 					<span className="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
