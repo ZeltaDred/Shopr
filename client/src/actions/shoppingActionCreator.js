b0adfec0360a13c95c0a17dc5e7bf48e4eefa7c7
Jake Duckett <Legac3e@gmail.com>
2016-07-13 14:37:23 -0600

    fixed actionCreator file

--- client/src/actions/shoppingActionCreator.js
+++ client/src/actions/shoppingActionCreator.js
@@ -7,19 +7,16 @@ var  API = require('../helpers/api');
 var ShoppingActionCreator = {
 	createStore: function (store) {
 		var newStorePromise = API.createStore(store);
+    
+    newStorePromise.
+			then( function(newStore) {
+				Dispatcher.dispatch({
+				actionType: ActionTypes.CREATE_STORE,
+				store: newStore				
+			});
 
-    // <<<<<<< HEAD
-    // newStorePromise.
-    //		then( function(newStore) {
-    // =======
-    //newStorePromise
-    // .then(function(newStore) {
-    // >>>>>>> origin/matt-branch
-    // 			Dispatcher.dispatch({
-    // 			actionType: ActionTypes.CREATE_STORE,
-    // 		store: newStore
-    //		});
-    //})
+		})
+
 		.fail(function(xhr, status, err) {
 			console.log("failed to create a store");
 		})
