b08fa67b67e82d9def05d18f7fc3f8604370a1a0
Jake Duckett <Legac3e@gmail.com>
2016-07-14 21:53:44 -0600

    added old commits as comments incase i need to backtrack

--- client/src/components/shopping/ShoppingListPage.js
+++ client/src/components/shopping/ShoppingListPage.js
@@ -60,7 +60,47 @@ var ShoppingListPage = React.createClass({
 	},
 
 	render: function() {
+    {/*		return (
+          {//	<div>}
+          {//<h2>{this.state.store.storeName}</h2>}
+        {//				<Link className="btn btn-primary btn-sm" to="/manage-section">}
+        { //Add Section &nbsp;}
+        {//		<span className="glyphicon glyphicon-plus" aria-hidden="true"></span>}
+        {//</Link>} 
+        
+			<form>
+				<TextInput
+					name="Section Name"
+					placeholder="Section Name"
+          value={this.state.store.storeName.storeSection}
+					saveStoreState={this.state.saveStoreState}
+          onChange = {this.saveTextInputState}
+				/>
 
+        <button className="btn btn-primary btn-sm" 
+          onClick={this.props.saveStore} value="save section">
+					Save Section &nbsp;
+					<span className="glyphicon glyphicon-save" aria-hidden="true"></span>
+				</button>
+
+
+				<Link className="btn btn-primary btn-sm" to="/choose-store">
+					Move Checked &nbsp;
+					<span className="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>
+				</Link>
+
+				<button className="btn btn-primary btn-sm">
+					Delete Checked &nbsp;
+					<span className="glyphicon glyphicon-trash" aria-hidden="true"></span>
+				</button>
+      </form>
+
+				<SectionList
+					store={this.state.store}
+				/>
+			</div>
+		);
+  */}
 		return (
 			<div>
 				<div className="container">
@@ -87,7 +127,7 @@ var ShoppingListPage = React.createClass({
 						onChange={this.saveSectionState}
 						/>
 						<button className="btn btn-primary btn-sm glyphicon glyphicon-plus" 
-						//onClick run a function
+              {//onClick run a function}
 						value="+">
 						</button>
 					</form>
